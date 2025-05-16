d75ff66f30256272ba89886ac767d8c7985ef65c
weichaoren <weichaoren@buaa.edu.cn>
2025-05-16 19:33:56 +0800

    add new code

--- eval/metric_lib/metric_mmlu.py
+++ eval/metric_lib/metric_mmlu.py
@@ -35,7 +35,7 @@ def judge_correctness_mmlu(problem, real_answer, generated_answer):
         return False
     generated_answer = generated_answer[pos:]
     generated_answer = generated_answer.replace("answer is", "Answer:")
-    ANSWER_PATTERN_MULTICHOICE = r"(?i)Answer\s*:\s*([A-Za-z])\)?\s*.*"
+    ANSWER_PATTERN_MULTICHOICE = r"(?i)Answer\s*:\s*(.*)"
     match_for_generated_answer = re.findall(ANSWER_PATTERN_MULTICHOICE, generated_answer)
     extracted_answer = match_for_generated_answer[-1] if match_for_generated_answer else None
     if extracted_answer is None:
